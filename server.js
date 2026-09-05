c55990c0d285e07ffef98bb80af9a1ccd696a726
krisaleth <kkimtam486@gmail.com>
2026-09-05 10:55:19 +0700

    add file to main branch

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import routes from './routes/index.js';
+const app = express();
+
+const PORT = process.env.PORT || 3000
+
+app.use(express.json());
+app.use('/api', routes);
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
