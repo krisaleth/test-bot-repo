c01fb46122e40db82c89bd8d0b60abe220388ff2
Krisaleth <kimtam486@gmail.com>
2026-09-19 20:52:32 +0700

    Remove routes middleware from server.js
    Removed routes import and middleware usage.

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 import express from 'express';
-import routes from './routes/index.js';
 const app = express();
 
 const PORT = process.env.PORT || 3000
 
 app.use(express.json());
-app.use('/api', routes);
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
